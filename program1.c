191f150fac3f2750a005f4adeb3f7e4c6023eea2
Sangam Gautam <sangamgautam10561@gmail.com>
2025-09-21 01:44:52 +0530

    Update and rename README.md to program1.c

---
+++ program1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("hello sanagam\nhow are you\nwhat are you doing\nwhat is your dream\nwhat are you doing in present time\nremember that you come to earth to do somthing big");
+   
+    return 0;
+}
+code no.2
+ #include<stdio.h>
+ int main()
+ {
