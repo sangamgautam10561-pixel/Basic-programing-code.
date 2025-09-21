1bb048c8c0626fa87248d0e80e9bd6fe40a357b7
Sangam Gautam <sangamgautam10561@gmail.com>
2025-09-22 00:50:50 +0530

    Create Percentage_1.c

---
+++ Percentage_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,c,d,e;
+    a=37;//maths
+    b=35;//chemistry
+    c=36;//physics
+    d=38;//english
+    e=39;//biology
+   float percent=(a+b+c+d+e)/200*100;
+    printf("percentage of 5 subject:%f",percent);
+    
+    return 0;
+}
